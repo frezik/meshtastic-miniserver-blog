7e48fc3ca1c84cf0a33a1baadb5dbf178762b2cf
tmurray <tmurray@wumpus-cave.net>
2025-04-19 11:19:23 -0500

    Simplify definitions for more generic code

--- src/packet.rs
+++ src/packet.rs
@@ -9,10 +9,42 @@ const PROTOCOL_VERSION_LE: u8 = 0x01;
 const PROTOCOL_VERSION: u16 = ((PROTOCOL_VERSION_BE as u16) << 8)
     | (PROTOCOL_VERSION_LE as u16);
 
-const PACKET_ID_REQUEST: u8 = 0x00;
+pub enum PacketType {
+    Request,
+}
 
+impl PacketType {
+    fn value( &self ) -> u8
+    {
+        match *self {
+            PacketType::Request => 0x00,
+        }
+    }
+}
 
 #[derive(Debug)]
 pub enum PacketError {
     PayloadTooLarge,
+}
+
+pub fn packet_to_vec(
+    packet_type: PacketType,
+    payload: Vec<u8>,
+) -> Result< Vec<u8>, PacketError >
+{
+    let payload_len = payload.len();
+    if payload_len > 255 {
+        return Err( PacketError::PayloadTooLarge );
+    }
+
+    let mut out_vec: Vec<u8> = vec![
+        MAGIC_NUM_BE,
+        MAGIC_NUM_LE,
+        PROTOCOL_VERSION_BE,
+        PROTOCOL_VERSION_LE,
+        packet_type.value(),
+        payload_len as u8,
+    ];
+    out_vec.extend( payload.clone() );
+    return Ok( out_vec );
 }
--- src/packet/request.rs
+++ src/packet/request.rs
@@ -2,44 +2,23 @@ use crate::packet;
 
 pub struct RequestPacket
 {
-    // TODO remove magic_num, version, and packet_type. Make payload_length
-    // and payload private fields
-    pub magic_num: u16,
-    pub version: u16,
-    pub packet_type: u8,
-    pub payload_length: u8,
-    pub payload: Vec<u8>,
+    resource_num: u16,
 }
 
 impl RequestPacket {
     pub fn new( resource_num: u16 ) -> Result<Self, packet::PacketError>
     {
-        let resource_num_big: u8 = ((resource_num >> 8) & 0xFF) as u8;
-        let resource_num_small: u8 = (resource_num & 0xFF) as u8;
-        let payload: Vec<u8> = vec![ resource_num_big, resource_num_small ];
-        let len: u8 = payload.len() as u8;
-
         Ok( Self {
-            magic_num: packet::MAGIC_NUM,
-            version: packet::PROTOCOL_VERSION,
-            packet_type: packet::PACKET_ID_REQUEST,
-            payload_length: len,
-            payload: payload,
+            resource_num: resource_num,
         })
     }
 
     pub fn to_vec( &self ) -> Vec<u8>
     {
-        let mut out_vec: Vec<u8> = vec![
-            packet::MAGIC_NUM_BE,
-            packet::MAGIC_NUM_LE,
-            packet::PROTOCOL_VERSION_BE,
-            packet::PROTOCOL_VERSION_LE,
-            self.packet_type,
-            self.payload_length,
-        ];
-        out_vec.extend( self.payload.clone() );
-        return out_vec;
+        let resource_num_big: u8 = ((self.resource_num >> 8) & 0xFF) as u8;
+        let resource_num_small: u8 = (self.resource_num & 0xFF) as u8;
+        let payload: Vec<u8> = vec![ resource_num_big, resource_num_small ];
+        packet::packet_to_vec( packet::PacketType::Request, payload ).unwrap()
    }
 }
 
@@ -54,10 +33,6 @@ mod tests {
         let pack_result = RequestPacket::new( 0xAB12 );
 
         assert_eq!( pack_result.is_ok(), true );
-
-        let pack = pack_result.unwrap();
-        assert_eq!( pack.payload_length, 2 );
-        assert_eq!( pack.payload, vec![ 0xAB, 0x12 ] );
     }
 
     #[test]
@@ -65,12 +40,13 @@ mod tests {
     {
         let pack = RequestPacket::new( 0xAB12 ).unwrap();
         let pack_vec = pack.to_vec();
+
         assert_eq!( pack_vec, vec![
             packet::MAGIC_NUM_BE,
             packet::MAGIC_NUM_LE,
             packet::PROTOCOL_VERSION_BE,
             packet::PROTOCOL_VERSION_LE,
-            packet::PACKET_ID_REQUEST,
+            packet::PacketType::Request.value(),
             2,
             0xAB,
             0x12,
