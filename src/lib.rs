9d71ab9a28344950a1a8bcd69decaf173d4478ef
tmurray <tmurray@wumpus-cave.net>
2025-04-19 10:49:57 -0500

    Serialize request packet

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
-const MAGIC_NUM: u16 = 0xBB50;
-const PROTOCOL_VERSION: u16 = 0x0001;
+const MAGIC_NUM_BE: u8 = 0xBB;
+const MAGIC_NUM_LE: u8 = 0x50;
+const MAGIC_NUM: u16 = ((MAGIC_NUM_BE as u16) << 8)
+    | (MAGIC_NUM_LE as u16);
+const PROTOCOL_VERSION_BE: u8 = 0x00;
+const PROTOCOL_VERSION_LE: u8 = 0x01;
+const PROTOCOL_VERSION: u16 = ((PROTOCOL_VERSION_BE as u16) << 8)
+    | (PROTOCOL_VERSION_LE as u16);
 const PACKET_ID_REQUEST: u8 = 0x00;
 
 
@@ -33,6 +39,20 @@ impl RequestPacket {
             payload: payload,
         })
     }
+
+    pub fn to_vec( &self ) -> Vec<u8>
+    {
+        let mut out_vec: Vec<u8> = vec![
+            MAGIC_NUM_BE,
+            MAGIC_NUM_LE,
+            PROTOCOL_VERSION_BE,
+            PROTOCOL_VERSION_LE,
+            self.packet_type,
+            self.payload_length,
+        ];
+        out_vec.extend( self.payload.clone() );
+        return out_vec;
+   }
 }
 
 
@@ -50,5 +70,22 @@ mod tests {
         let pack = pack_result.unwrap();
         assert_eq!( pack.payload_length, 2 );
         assert_eq!( pack.payload, vec![ 0xAB, 0x12 ] );
+    }
+
+    #[test]
+    fn request_packet_deserialize()
+    {
+        let pack = RequestPacket::new( 0xAB12 ).unwrap();
+        let pack_vec = pack.to_vec();
+        assert_eq!( pack_vec, vec![
+            MAGIC_NUM_BE,
+            MAGIC_NUM_LE,
+            PROTOCOL_VERSION_BE,
+            PROTOCOL_VERSION_LE,
+            PACKET_ID_REQUEST,
+            2,
+            0xAB,
+            0x12,
+        ]);
     }
 }
