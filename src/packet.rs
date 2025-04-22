47ecb87cfcb640dd10293c001d891cff9dd95a46
tmurray <tmurray@wumpus-cave.net>
2025-04-22 16:29:17 -0500

    Deserialize request packets

--- src/packet.rs
+++ src/packet.rs
@@ -1,3 +1,8 @@
+use crate::packet::article_response::ArticleResponsePacket;
+use crate::packet::directory_response::DirectoryResponsePacket;
+use crate::packet::error_response::ErrorResponsePacket;
+use crate::packet::request::RequestPacket;
+
 pub mod article_response;
 pub mod directory_response;
 pub mod error_response;
@@ -6,14 +11,17 @@ pub mod request;
 
 const MAGIC_NUM_BE: u8 = 0xBB;
 const MAGIC_NUM_LE: u8 = 0x50;
-#[allow(dead_code)]
 const MAGIC_NUM: u16 = ((MAGIC_NUM_BE as u16) << 8)
     | (MAGIC_NUM_LE as u16);
 const PROTOCOL_VERSION_BE: u8 = 0x00;
 const PROTOCOL_VERSION_LE: u8 = 0x01;
-#[allow(dead_code)]
 const PROTOCOL_VERSION: u16 = ((PROTOCOL_VERSION_BE as u16) << 8)
     | (PROTOCOL_VERSION_LE as u16);
+
+const PACKET_TYPE_REQUEST: u8 = 0x00;
+const PACKET_TYPE_DIRECTORY_RESPONSE: u8 = 0x01;
+const PACKET_TYPE_ARTICLE_RESPONSE: u8 = 0x02;
+const PACKET_TYPE_ERROR_RESPONSE: u8 = 0x03;
 
 /// Defines the different types of packets
 pub enum PacketType {
@@ -28,10 +36,10 @@ impl PacketType {
     fn value( &self ) -> u8
     {
         match *self {
-            PacketType::Request => 0x00,
-            PacketType::DirectoryResponse => 0x01,
-            PacketType::ArticleResponse => 0x02,
-            PacketType::ErrorResponse => 0x03,
+            PacketType::Request => PACKET_TYPE_REQUEST,
+            PacketType::DirectoryResponse => PACKET_TYPE_DIRECTORY_RESPONSE,
+            PacketType::ArticleResponse => PACKET_TYPE_ARTICLE_RESPONSE,
+            PacketType::ErrorResponse => PACKET_TYPE_ERROR_RESPONSE,
         }
     }
 }
@@ -39,7 +47,20 @@ impl PacketType {
 #[derive(Debug)]
 /// Different types of errors
 pub enum PacketError {
-    PayloadTooLarge,
+    MalformedPacket( String ),
+    ProtocolVersion( u16 ),
+    UnknownPacketType( u8 ),
+    /// Payload too big. First number is the limit, second number is how big
+    /// the payload is.
+    PayloadTooLarge( u8, u32 ),
+}
+
+/// Different types of packeets
+pub enum PacketResult {
+    Request( RequestPacket ),
+    DirectoryResponse( DirectoryResponsePacket ),
+    ArticleResponse( ArticleResponsePacket ),
+    ErrorResponse( ErrorResponsePacket ),
 }
 
 /// Transforms a packet type, payload, and connection ID into a vector of 
@@ -55,7 +76,7 @@ pub fn packet_to_vec(
 {
     let payload_len = payload.len();
     if payload_len > 255 {
-        return Err( PacketError::PayloadTooLarge );
+        return Err( PacketError::PayloadTooLarge( 255, payload_len as u32 ) );
     }
 
     let mut out_vec: Vec<u8> = vec![
@@ -71,6 +92,52 @@ pub fn packet_to_vec(
     out_vec.extend( payload.clone() );
 
     return Ok( out_vec );
+}
+
+
+/// Transforms a vector of bytes into a packet object
+pub fn vec_to_packet(
+    vec_packet: Vec<u8>,
+) -> Result< PacketResult, PacketError >
+{
+    if 8 > vec_packet.len() {
+        return Err( PacketError::MalformedPacket(
+            "Packet too short".to_string() ) );
+    }
+
+    let magic_num = ((vec_packet[0] as u16) << 8) | (vec_packet[1] as u16);
+    if MAGIC_NUM != magic_num {
+        return Err( PacketError::MalformedPacket(
+            "Unrecognized magic number".to_string() ) );
+    }
+
+    let protocol_version = ((vec_packet[2] as u16) << 8)
+        | (vec_packet[3] as u16);
+    if PROTOCOL_VERSION < protocol_version {
+        return Err( PacketError::ProtocolVersion( PROTOCOL_VERSION ) );
+    }
+
+    let packet_type = vec_packet[4];
+    match packet_type {
+        PACKET_TYPE_REQUEST => Ok(
+            PacketResult::Request(
+                RequestPacket::from_vec( vec_packet ).unwrap()
+            )
+        ),
+        // TODO all of these
+        //PACKET_TYPE_DIRECTORY_RESPONSE => Ok(
+        //    DirectoryResponsePacket::from_vec( vec_packet )
+        //),
+        //PACKET_TYPE_ARTICLE_RESPONSE => Ok(
+        //    ArticleResponsePacket::from_vec( vec_packet )
+        //),
+        //PACKET_TYPE_ERROR_RESPONSE => Ok(
+        //    ErrorResponsePacket::from_vec( vec_packet )
+        //),
+        _ => Err(
+            PacketError::UnknownPacketType( packet_type ) 
+        ),
+    }
 }
 
 
--- src/packet/request.rs
+++ src/packet/request.rs
@@ -9,6 +9,9 @@ pub struct RequestPacket
 {
     /// The ID of the directory entry to request
     resource_num: u16,
+    /// The connection ID. This isn't set if we're constructing a fresh 
+    /// request packet, but will be set if we're building from a vec.
+    connection_id: Option< u16 >,
 }
 
 impl RequestPacket {
@@ -17,6 +20,30 @@ impl RequestPacket {
     {
         Ok( Self {
             resource_num: resource_num,
+            connection_id: None,
+        })
+    }
+
+    /// Convert the vector of bytes into a packet object
+    pub fn from_vec(
+        vec_packet: Vec<u8>
+    ) -> Result<Self, packet::PacketError> {
+        if 10 != vec_packet.len() {
+            return Err(
+                packet::PacketError::MalformedPacket(
+                    "Packet too large".to_string()
+                )
+            );
+        }
+
+        let connection_id: u16 = ((vec_packet[5] as u16) << 8)
+            | (vec_packet[6] as u16);
+        let resource_num: u16 = ((vec_packet[8] as u16) << 8)
+            | (vec_packet[9] as u16);
+
+        Ok( Self {
+            resource_num: resource_num,
+            connection_id: Some( connection_id ),
         })
     }
 
@@ -27,8 +54,13 @@ impl RequestPacket {
         let resource_num_big: u8 = ((self.resource_num >> 8) & 0xFF) as u8;
         let resource_num_small: u8 = (self.resource_num & 0xFF) as u8;
 
-        let mut rng = SmallRng::from_rng( &mut rand::rng() );
-        let connection_id: u16 = rng.random();
+        let connection_id = match self.connection_id {
+            Some(id) => id,
+            None => {
+                let mut rng = SmallRng::from_rng( &mut rand::rng() );
+                rng.random()
+            },
+        };
 
         let payload: Vec<u8> = vec![ resource_num_big, resource_num_small ];
         packet::packet_to_vec(
@@ -53,7 +85,7 @@ mod tests {
     }
 
     #[test]
-    fn request_packet_deserialize()
+    fn request_packet_serialize()
     {
         let pack = RequestPacket::new( 0xAB12 ).unwrap();
         let pack_vec = pack.to_vec();
@@ -75,5 +107,17 @@ mod tests {
         ].clone() );
 
         assert_eq!( pack_vec, expect_vec );
+    }
+
+    #[test]
+    fn request_packet_deserialize()
+    {
+        let mut pack = RequestPacket::new( 0xAB12 ).unwrap();
+        pack.connection_id = Some( 0xEF98 );
+        let pack_vec = pack.to_vec();
+
+        let new_pack = RequestPacket::from_vec( pack_vec ).unwrap();
+        assert_eq!( pack.connection_id, new_pack.connection_id );
+        assert_eq!( pack.resource_num, pack.resource_num );
     }
 }
